1608244676ffd5fb8f304e8327dfb5f661042b0a
mariescheffmann <mariemadsen26@yahoo.dk>
2024-04-23 10:10:01 +0200

    Saves all new lightnings in the database

--- SCPSMars/src/main/java/org/example/Database.java
+++ SCPSMars/src/main/java/org/example/Database.java
@@ -1,4 +1,86 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Database {
+    static Connection connection = null;
+    static Dotenv dotenv = Dotenv.load();
+
+    public void setup() {
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/lightning","postgres", dotenv.get("password"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void fetch() {
+
+        // querry database for users with cpr x
+        try {
+            PreparedStatement querryStatement = connection.prepareStatement("SELECT * FROM lightningdb.type");
+            ResultSet querryResultSet = querryStatement.executeQuery();
+
+            System.out.println("The following matched the querry:");
+            while (querryResultSet.next()){
+                System.out.println("type: "+querryResultSet.getString("type_name"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public int saveDate(LocalDate date) {
+        try {
+            PreparedStatement querryStatement = connection.prepareStatement("SELECT * FROM lightningdb.day WHERE date = ?");
+            querryStatement.setDate(1, Date.valueOf(date));
+            ResultSet querryResultSet = querryStatement.executeQuery();
+
+            if (!querryResultSet.next()) {
+                // Inserting the new day in the database
+                PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO lightningdb.day (date, total_lightnings) VALUES (?, ?)");
+                insertStatement.setDate(1, Date.valueOf(date));
+                insertStatement.setInt(2,1);
+                insertStatement.execute();
+            } else {
+                // Updating the amount of lightnings for the already existing day
+                int lightnings = querryResultSet.getInt("total_lightnings");
+                PreparedStatement insertStatement = connection.prepareStatement("UPDATE lightningdb.day SET total_lightnings = ? WHERE date=?");
+                insertStatement.setInt(1,(lightnings+1));
+                insertStatement.setDate(2, Date.valueOf(date));
+                insertStatement.execute();
+            }
+
+            // Returning the id for the given date
+            PreparedStatement querryForId = connection.prepareStatement("SELECT * FROM lightningdb.day WHERE date = ?");
+            querryForId.setDate(1, Date.valueOf(date));
+            ResultSet resultSetDate = querryForId.executeQuery();
+            resultSetDate.next();
+            return resultSetDate.getInt("id");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insertIntoDB(LocalDate date, LocalDateTime localDateTime, int type) {
+        // insert lightning into database
+        int dayId = saveDate(date);
+
+        try {
+            PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO lightningdb.lightning (timestamp, day_id, type_id) VALUES (?,?,?)");
+            insertStatement.setTimestamp(1, Timestamp.valueOf(localDateTime));
+            insertStatement.setInt(2, dayId);
+            insertStatement.setInt(3, type);
+            insertStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- SCPSMars/src/main/java/org/example/Main.java
+++ SCPSMars/src/main/java/org/example/Main.java
@@ -1,22 +1,29 @@
 package org.example;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.json.simple.JSONObject;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Main {
     static Dotenv dotenv = Dotenv.load();
-    private static String API_PARAMETERS = "period=latest-month&";
+    private static String API_PARAMETERS = "period=latest-day&";
     private static String API = String.format("https://dmigw.govcloud.dk/v2/lightningdata/collections/observation/items?%sapi-key=%s", API_PARAMETERS, dotenv.get("API_KEY"));
 
     public static void main(String[] args) {
+        Database db = new Database();
+        db.setup();
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(API))
                 .method("GET", HttpRequest.BodyPublishers.noBody())
@@ -29,10 +36,19 @@ public class Main {
             e.printStackTrace();
         }
 
-        JsonObject j = new JsonParser().parse(response.body()).getAsJsonObject();
-        JSONObject obj = new JSONObject(j.asMap());
+        JsonObject jsonObject = JsonParser.parseString(response.body()).getAsJsonObject();
+        JsonArray jsonArray = jsonObject.getAsJsonArray("features");
 
-        System.out.println(obj.get("numberReturned"));
+        for (int i=0; i<jsonArray.size(); i++) {
+            JsonObject properties = jsonArray.get(i).getAsJsonObject().get("properties").getAsJsonObject();
 
+            int type = properties.get("type").getAsInt();
+            String timeString = properties.get("observed").toString().replace("\"", "");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'", Locale.ENGLISH);
+            LocalDateTime dateTime = LocalDateTime.parse(timeString, formatter);
+            LocalDate date = dateTime.toLocalDate();
+
+            db.insertIntoDB(date, dateTime, type);
+        }
     }
 }
