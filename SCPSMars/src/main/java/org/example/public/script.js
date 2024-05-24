b0fab7b232a7ad47ddc50064b13b21a5991df7ef
mariescheffmann <mariemadsen26@yahoo.dk>
2024-05-24 11:58:26 +0200

    fetchers fix

--- SCPSMars/src/main/java/org/example/public/script.js
+++ SCPSMars/src/main/java/org/example/public/script.js
@@ -11,3 +11,39 @@ export const fetchDay = () => {
             throw error;
         });
 };
+
+export const fetchWeek = () => {
+    return fetch(url + '/api/week')
+        .then(response => {
+            console.log('Response:', response);
+            return response.text();
+        })
+        .catch(error => {
+            console.error('Error fetching state:', error);
+            throw error;
+        });
+};
+
+export const fetchCloudToGround = () => {
+    return fetch(url + '/api/cloudToGround')
+        .then(response => {
+            console.log('Response:', response);
+            return response.text();
+        })
+        .catch(error => {
+            console.error('Error fetching state:', error);
+            throw error;
+        });
+};
+
+export const fetchCloudToClous = () => {
+    return fetch(url + '/api/cloudToCloud')
+        .then(response => {
+            console.log('Response:', response);
+            return response.text();
+        })
+        .catch(error => {
+            console.error('Error fetching state:', error);
+            throw error;
+        });
+};
